4cb0988c720747afb45897ef3922bba97fb07bf3
RV-Coder01 <rvcoder01@gmail.com>
2026-05-07 13:36:47 +0530

    localStorage changes

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const text=document.getElementById("input-txt");
 let inputButton= document.getElementById("save-btn");
 const ulEl=document.getElementById("ul-el");
+let myLeads=[];
 
-inputButton.addEventListener("click", function(){
-    let myLeads=[];
-    myLeads.push(text.value);
+let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
     renderLead(myLeads);
-    text.value="";
- })
+}
+inputButton.addEventListener("click", function(){
+        myLeads.push(text.value);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLead(myLeads);
+        text.value="";
+ }
+)
 
 
 function renderLead(myLeads){
+    let listItems = "";
     for(let i=0;i<myLeads.length;i++){
-        ulEl.innerHTML +=
+        listItems +=
         `<li>
             <a target='_blank' href='${myLeads[i]}'>
                 ${myLeads[i]} 
             </a>
         </li>`;
     }
+    ulEl.innerHTML =listItems
 }
 
 
